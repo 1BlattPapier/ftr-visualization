cd91f340fea6c3e9b6d1dc0712a393306e6377cc
Simon Lämmer <simon.laemmer@outlook.de>
2022-08-08 09:50:37 +0200

    Add proof of concept graph.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import datetime
+import json
+from lib2to3.pgen2.pgen import DFAState
 from flask import Flask, request, render_template, jsonify
 
 import pandas
-import sklearn
+from sklearn.manifold import TSNE
 
 from db import DB
 
@@ -15,28 +17,35 @@ db = DB()
 @app.get("/test")
 def get_test():
     args = request.args
-    results = db.filterStatements(
+    db_results = db.filterStatements(
         datetime.datetime(int(args.get('st_year')), 12, 1),
         datetime.datetime(int(args.get('end_year')), 12, 1)
     )
 
-    X = []
-    Y = []
-    for result in results:
-        topic = result['topic']
+    data = []
+    for result in db_results:
+        topic_dict = {k: v for k, v in zip(result['topic'], result['topic_certainty'])}
         emotion_dict = {k: v for k, v in zip(result['emotion'], result['emotion_certainty'])}
-        Y.append(topic)
-        X.append(emotion_dict)
-    x_df = pandas.DataFrame.from_records(X)
-    x_df = x_df.fillna(0)
-    print(x_df.values.shape)
-    print(Y)
-    print(x_df.head())
+        topic_dict.update(emotion_dict)
+        data.append(topic_dict)
+    data_df = pandas.DataFrame.from_records(data)
+    data_df = data_df.fillna(0)
 
-    file = open('altair-data-ad5eabdff90a827509b12a9aaa59bbfe.json')
-    json = jsonify(next(file))
+    tsne = TSNE()
+    tsne_results = tsne.fit_transform(data_df)
 
-    return json
+    graph_data = []
+    for data_entry, tsne_result, db_entry in zip(data, tsne_results, db_results):
+        data_entry.update(
+            {
+                'text': db_entry['text'],
+                'x': str(tsne_result[0]),
+                'y': str(tsne_result[1]),
+            }
+        )
+        graph_data.append(data_entry)
+
+    return json.dumps(graph_data)
 
 @app.get('/')
 def get_scatter_plot():
